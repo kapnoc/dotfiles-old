8efcf63a5f62508023efffa6bd5b3fd1d27c8143
Tanguy Gérôme <tanguy.gerome@epitech.eu>
2017-07-14 14:09:47 +0200

    +nm-applet

--- sh/herbstluftwm.sh
+++ sh/herbstluftwm.sh
@@ -4,7 +4,7 @@
 
 install_herbstluft()
 {
-	@ sudo $PKG herbstluftwm conky rofi trayer-srg xset dzen2 feh
+	@ sudo $PKG herbstluftwm conky rofi trayer-srg xset dzen2 feh network-manager-applet
 	@ mkdir -p ~/.config
 	@ cp -r files/herbstluftwm ~/.config/
 	echo "herbstluftwm installed"
