ed8680da70ba9a8d77a102e894005f9e6f0b01eb
Karl Toffel <kapnoc@memeware.net>
2017-07-16 21:34:14 +0200

    +wallpaper

--- sh/herbstluftwm.sh
+++ sh/herbstluftwm.sh
@@ -7,6 +7,7 @@ install_herbstluft()
 	@ sudo $PKG herbstluftwm conky rofi trayer-srg xset dzen2 feh network-manager-applet
 	@ mkdir -p ~/.config
 	@ cp -r files/herbstluftwm ~/.config/
+	@ cp -f files/Pictures ~/
 	echo "herbstluftwm installed"
 	echo
 }
