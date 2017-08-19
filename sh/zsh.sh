1167bd939ed0a8b5a7237dbe112406ecd8911793
Karl Toffel <kapnoc@memeware.net>
2017-08-19 09:47:02 +0200

    +untracked omzi install

--- sh/zsh.sh
+++ sh/zsh.sh
@@ -5,7 +5,7 @@
 install_zsh()
 {
 	@ sudo $PKG zsh zsh-completions zsh-syntax-highlighting python-Pygments
-	@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 	@ git clone git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
 	# add generic zshrc
 	@ cp files/.zshrc ~/
