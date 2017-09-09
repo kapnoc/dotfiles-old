d5d508738c4cbc3f95425ebd815a66e0a7b2bfd3
Karl Toffel <kapnoc@memeware.net>
2017-09-09 11:40:52 +0200

    Added Gentoo's root bashrc

--- sh/zsh.sh
+++ sh/zsh.sh
@@ -7,7 +7,7 @@ install_zsh()
 	@ sudo $PKG zsh zsh-completions zsh-syntax-highlighting python-Pygments
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 	@ git clone git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
-	# add generic zshrc
+	# TODO: add generic zshrc
 	@ sed -e "s/kapnoc/$USER/g" files/.zshrc > ~/.zshrc
 	echo "zsh installed"
 	echo
