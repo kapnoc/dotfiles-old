2be71a3ca0f6923e64bc32d69b4bdf3d0d47ee20
Tanguy GEROME <tanguy.gerome@epitech.eu>
2018-02-01 15:26:09 +0100

    added small scripts

---
+++ files/bin/aled-class.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env sh
+
+
+touch "$1.hpp"
+touch "$1.cpp"
+
+ALED="`echo "$1.hpp" | tr '[:lower:]' '[:upper:]' | tr '.' '_'`_"
+
+echo "#ifndef $ALED" >> "$1.hpp"
+echo "	#define $ALED" >> "$1.hpp"
+echo >> "$1.hpp"
+echo "class $1 {" >> "$1.hpp"
+echo "public:" >> "$1.hpp"
+echo "	$1();" >> "$1.hpp"
+echo "	~$1();" >> "$1.hpp"
+echo >> "$1.hpp"
+echo "protected:" >> "$1.hpp"
+echo >> "$1.hpp"
+echo "private:" >> "$1.hpp"
+echo "};" >> "$1.hpp"
+echo >> "$1.hpp"
+echo "#endif /* !$ALED */" >> "$1.hpp"
+
+echo "#include \"$1.hpp\"" >> "$1.cpp"
+echo >> "$1.cpp"
+echo "$1::$1()" >> "$1.cpp"
+echo "{" >> "$1.cpp"
+echo "}" >> "$1.cpp"
+echo >> "$1.cpp"
+echo "$1::~$1()" >> "$1.cpp"
+echo "{" >> "$1.cpp"
+echo "}" >> "$1.cpp"
+
+mv "$1.hpp" "inc/"
+mv "$1.cpp" "inc/"
